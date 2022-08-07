6764443b46d646b9c44eb460c8df774df2c1ebfa
Jonathan Zanella <jonathanrafaelz@gmail.com>
2022-08-07 17:14:06 -0300

    create client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { ApolloProvider } from "@apollo/client";
+import createApolloClient from "./utils/createApolloClient";
 
 function App() {
-  return <div>App</div>;
+  const [token, setToken] = useState(() => localStorage.get("token"));
+
+  const apolloClient = useMemo(
+    () =>
+      createApolloClient({
+        getToken: () => token,
+        onUnauthorized: () => setToken(null),
+      }),
+    [token]
+  );
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <div>App</div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
