17291c684d5b64ed8dc5bcb7d881170fb9b00847
Jonathan Zanella <jonathanrafaelz@gmail.com>
2022-08-10 09:16:33 -0300

    show months

--- src/pages/Main/Content.tsx
+++ src/pages/Main/Content.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useMemo } from "react";
+import styled from "styled-components";
 import { gql, useQuery } from "@apollo/client";
+import moment from "moment";
+
+const Table = styled.table`
+  padding: 10px;
+  border: 1px solid black;
+`;
+
+interface Income {
+  uuid: string;
+  date: number;
+  yield: number;
+  value: number;
+}
+
+interface Investment {
+  uuid: string;
+  name: string;
+  type: string;
+  holder: string;
+  objective: string;
+  incomes: [Income];
+}
+
+const MONTHS = generateMonths();
+
+function generateMonths() {
+  const date = moment();
+
+  return new Array(60).fill(0).map((_) => {
+    const formattedDate = date.format("MM/YY");
+    date.subtract(1, "months");
+    return formattedDate;
+  });
+}
+
+console.log({ MONTHS });
 
 const GET_ALL_INVESTMENTS = gql`
   {
@@ -13,7 +50,6 @@ const GET_ALL_INVESTMENTS = gql`
         uuid
         date
         yield
-        received
         value
       }
     }
@@ -23,13 +59,40 @@ const GET_ALL_INVESTMENTS = gql`
 function Content() {
   const { loading, error, data } = useQuery(GET_ALL_INVESTMENTS);
 
-  console.log({ data });
+  const parsedData = useMemo(() => {
+    if (!data) return;
+
+    return data.investments.map((investment: Investment) => {
+      const output = { ...investment, incomes: {} };
+      investment.incomes.forEach((income: Income) => {
+        // @ts-ignore
+        output.incomes[moment(income.date * 1000).format("MM/YY")] = {
+          ...income,
+          percent: income.yield / (income.value - income.yield),
+        };
+      });
+      return output;
+    });
+  }, [data]);
+
+  console.log({ parsedData });
 
   return (
     <div>
       {loading && "Loading..."}
       {error && error.message}
-      Blah
+      {parsedData && (
+        <Table>
+          <thead>
+            <tr>
+              <th>Investment</th>
+              {MONTHS.map((month) => (
+                <th key={month}>{month}</th>
+              ))}
+            </tr>
+          </thead>
+        </Table>
+      )}
     </div>
   );
 }
