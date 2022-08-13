8767925d0dec34c612a9750c13e52d4dd2096020
Jonathan Zanella <jonathanrafaelz@gmail.com>
2022-08-13 08:17:03 -0300

    design improvements

--- src/pages/Main/Content.tsx
+++ src/pages/Main/Content.tsx
@@ -1,24 +1,41 @@
 import { useMemo, useState } from "react";
 import styled from "styled-components";
 import uniq from "lodash/uniq";
+import sortBy from "lodash/sortBy";
 import Select from "react-select";
 import { gql, useQuery } from "@apollo/client";
 import moment from "moment";
 import numbro from "numbro";
 
 const Table = styled.table`
-  border: 1px solid black;
   white-space: nowrap;
-
-  td,
-  th {
-    border-right: 1px solid black;
-    border-bottom: 1px solid black;
-  }
+  border-collapse: collapse;
+  margin-top: 20px;
 
   tr:nth-child(even) {
-    background-color: #dddddd;
+    background-color: #f8f6ff;
   }
+`;
+
+const TableHeader = styled.th`
+  position: sticky;
+  top: 0;
+  box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+
+  font-size: 18px;
+  color: #fff;
+  line-height: 1.4;
+  background-color: #6c7ae0;
+
+  padding: 4px 8px;
+`;
+
+const TableData = styled.td`
+  font-size: 15px;
+  color: #808080;
+  line-height: 1.4;
+
+  padding: 4px 8px;
 `;
 
 interface Income {
@@ -74,33 +91,34 @@ function Content() {
   const parsedData = useMemo(() => {
     if (!data) return;
 
-    return data.investments
-      .filter((investment: Investment) => {
-        if (!filterType) return true;
+    return sortBy(
+      data.investments
+        .filter((investment: Investment) => {
+          if (!filterType) return true;
 
-        return filterType === investment.type;
-      })
-      .map((investment: Investment) => {
-        const output = { ...investment, incomes: {} };
-        investment.incomes.forEach((income: Income) => {
-          if (income.value > 0) {
-            // @ts-ignore
-            output.incomes[moment(income.date * 1000).format("MM/YY")] = {
-              ...income,
-              percent: income.yield / (income.value - income.yield),
-            };
-          }
-        });
-        return output;
-      });
+          return filterType === investment.type;
+        })
+        .map((investment: Investment) => {
+          const output = { ...investment, incomes: {} };
+          investment.incomes.forEach((income: Income) => {
+            if (income.value > 0) {
+              // @ts-ignore
+              output.incomes[moment(income.date * 1000).format("MM/YY")] = {
+                ...income,
+                percent: income.yield / (income.value - income.yield),
+              };
+            }
+          });
+          return output;
+        }),
+      "name"
+    );
   }, [data, filterType]);
 
   const types = useMemo(
     () => data && uniq(data.investments.map((i: Investment) => i.type)).sort(),
     [data]
   );
-
-  console.log({ types });
 
   return (
     <div>
@@ -119,9 +137,9 @@ function Content() {
           <Table>
             <thead>
               <tr>
-                <th>Investment</th>
+                <TableHeader>Investment</TableHeader>
                 {MONTHS.map((month) => (
-                  <th key={month}>{month}</th>
+                  <TableHeader key={month}>{month}</TableHeader>
                 ))}
               </tr>
             </thead>
@@ -129,16 +147,16 @@ function Content() {
               {/* @ts-ignore */}
               {parsedData.map((investment) => (
                 <tr key={investment.uuid}>
-                  <td>{investment.name}</td>
+                  <TableData>{investment.name}</TableData>
                   {MONTHS.map((month) => (
-                    <td key={month}>
+                    <TableData key={month}>
                       {investment.incomes[month]
                         ? numbro(investment.incomes[month].percent).format({
                             output: "percent",
                             mantissa: 2,
                           })
-                        : "-"}
-                    </td>
+                        : ""}
+                    </TableData>
                   ))}
                 </tr>
               ))}
